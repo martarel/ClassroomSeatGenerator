e16509f035b10159520183e9e69ebb4a67dc21a3
Marta Religa <59261954+martarel@users.noreply.github.com>
2021-12-30 13:11:05 +0100

    Update views.py

--- classroom/views.py
+++ classroom/views.py
@@ -6,15 +6,16 @@ def home(request):
     return render(request, 'classroom/home.html')
 
 def a23(request, klass = "NA20MA"):
-    students = Student.objects.order_by('?').filter(group = klass)
+    students = list(Student.objects.filter(group = klass)) 
+    shuffle(students)
     context = {
         'students': students
     }
     return render(request, 'classroom/a23.html', context)
 
 def b31(request, klass = "NA20MA"):
-    students = Student.objects.order_by('?').filter(group = klass)
-
+    students = list(Student.objects.filter(group = klass)) 
+    shuffle(students)
 
     context = {
         'students': students
@@ -22,9 +23,8 @@ def b31(request, klass = "NA20MA"):
     return render(request, 'classroom/b31.html', context)
 
 def b12(request, klass = "NA20MA"):
-    students1 = Student.objects.order_by('?').filter(group = klass)
-    students = list(students1)
-    students = shuffle(students)
+    students = list(Student.objects.filter(group = klass)) 
+    shuffle(students)
     context = {
         'students': students
     }
